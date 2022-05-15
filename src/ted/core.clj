b1cfcce9132a12a6519a6c5ad8d2a930bf90fec1
FrankApiyo <franklineapiyo@gmail.com>
2022-05-15 22:42:15 +0300

    Add sql functions to create user relation and user queries

--- src/ted/core.clj
+++ src/ted/core.clj
@@ -3,5 +3,15 @@
 
 (defn -main
   "I don't do a whole lot ... yet."
-  [& args]
+  [& _args]
   (println "Hello, World!"))
+
+;; (hugsql/def-db-fns "ted/sql/user.sql")
+;; (create-user-table! db)
+;; (add-user! db  {:username "apiyo" :password "ihavesecretsbutnotsomany"})
+;; (add-user-returning! db {:id "hug2" :password "return"})
+;; (add-users! db {:users [["bob" "Bob"] ["alice" "Alice"]]})
+;; (find-user db {:username "bob"})
+;; (find-user db {:username "apiyo"})
+;; (find-by-matching-ids db {:usernames ["alice" "bob" "nobody"]})
+;; (find-by-matching-ids db {:usernames ["alice" "bob" "nobody" "apiyo"]})
--- src/ted/sql/user.sql
+++ src/ted/sql/user.sql
@@ -1,5 +1,31 @@
--- :name add-user! :! :n
+-- :name add-user! :i :n
 -- :doc adds a new user
-INSERT INTO users
-(id, pass)
-VALUES (:id, :pass)
+INSERT INTO "user" (username, password)
+VALUES (:username, :password);
+-- :name create-user-table! :!
+CREATE TABLE "user" (
+    id_num SERIAL PRIMARY KEY,
+    username VARCHAR (40),
+    password VARCHAR (40)
+);
+-- :name add-user-returning! :i :1
+-- :doc adds a new user returning the id
+INSERT INTO "user" (username, password)
+VALUES(:id, :password)
+returning id_num;
+-- :name add-users! :! :n
+-- :doc add multiple users
+INSERT INTO "user" (username, password)
+VALUES :t * :users;
+-- :name find-user :? :1
+-- :doc find the user with the matching ID
+SELECT *
+FROM "user"
+WHERE username = :username;
+-- :name find-by-matching-ids :? :*
+-- :doc find users with matching IDs
+SELECT *
+FROM "user"
+WHERE username IN (:v * :usernames);
+-- :name drop-user-table! :!
+DROP TABLE "user";
